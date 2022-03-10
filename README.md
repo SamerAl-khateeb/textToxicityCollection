4f3953adc961d9108fa95815607d07ab3b89243c
Samer Al-khateeb <sxalkhateeb@ualr.edu>
2022-03-10 14:07:08 -0600

    added script file and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # textToxicityCollection
 This repository has Python script that can be used to collect the toxicity score of a given text (e.g., Tweets) using Google Perspective API
+
+## getToxicityScores.py
+This script reads each row in the **input_file.csv** file, take the second column which contain the text, make a request to Google Perspective API to determine the text toxicity, then write the output to another CSV file called **outputWithToxicityScoreDateofToday.csv**
+
+### Input & Output
+Input: **input_file.csv** this sample file contain the expected file format which will work with the provided code without any code modification. However, if the input file change in format, the Python script need to be updated as well to reflect the change in the input file, e.g., this file assume the second column contain the text, if the text is not in the second column, the code might not work or give incorrect results.
+
+Output: **outputWithToxicityScoreDateofToday.csv** this file will be generated one the code successfully run. it will contan the input file columns + one column containing the toxcity score as returned by Google Perspective API. The score in range on 0-1, 1 represnet the maximum toxicty score (given to very toxic texts).
---
+++ getToxicityScores.py
@@ -0,0 +1,124 @@
+# getToxicityScores.py                 By: Samer Al-khateeb
+# a script that can read text from a CSV file and 
+# return the toxicity score of the text as calculated 
+# by Google Perspective API (https://www.perspectiveapi.com)
+
+# To be able to run the code using IDLE you have to do the following 
+# (if you never did it before):
+# For Mac users, open terminal and type:
+#   python3 -m pip install --upgrade pip==19.0.3
+#   python3 -m pip install google-auth-oauthlib
+#   python3 -m pip install --upgrade google-api-python-client
+
+# For Windows users, open CMD and type:
+#   py -m pip install --upgrade pip==19.0.3
+#   py -m pip install google-auth-oauthlib
+#   py -m pip install --upgrade google-api-python-client
+
+
+from googleapiclient import discovery
+from datetime import datetime
+import time  
+import csv
+import json
+
+
+def write_output_to_CSV(biglist):
+    # get the current date so we can 
+    # append it to the CSV output file name
+    now = datetime.now()
+    dateAsString = now.strftime("%m-%d-%Y")
+    # name of csv file
+    filename = "outputWithToxicityScore{}.csv".format(dateAsString)
+
+    # column names
+    columnNames = ["ID", "Text", "ToxicityScore"]
+
+    # data rows of csv file
+    rows = biglist
+
+    # creating a file to save the output
+    with open(filename, 'w', newline='', encoding='utf-8') as csvOutputFile:
+        #creating a csv writer object 
+        csvwriter = csv.writer(csvOutputFile, delimiter=',', lineterminator='\n')
+        #write the columns headers
+        csvwriter.writerow(columnNames)
+        csvwriter.writerows(rows)
+    
+
+def main():
+    # paste your own API key below
+    apiKey = 'pasteYourAPIKeyHere!'
+
+    client = discovery.build(
+        "commentanalyzer", 
+        "v1alpha1", 
+        developerKey = apiKey, 
+        discoveryServiceUrl = "https://commentanalyzer.googleapis.com/$discovery/rest?version=v1alpha1", 
+        )
+
+    # an input file contain the text we want to calculate its toxicity score
+    fileName = "input_file.csv"
+    
+    # list to hold all the info that will be written later to a file
+    CSVOutputList = []
+
+    # open the file
+    with open(fileName, 'r', encoding='utf-8') as csvInputFile:
+        # read the CSV file
+        inputFileContents = csv.reader(csvInputFile)
+        # skip first row, i.e., read the column headers first
+        header = next(inputFileContents)
+
+        # Iterate over each row after the header in the csv
+        for row in inputFileContents:
+
+            # the first column is the row ID
+            rowID = row[0]
+
+            # the second column is the text, change it if otherwise
+            columnContainTheText = row[1]
+            
+            try:
+                analyze_request = { 'comment': { 'text': columnContainTheText }, 'requestedAttributes': {'TOXICITY': {}}}
+                response = client.comments().analyze(body=analyze_request).execute()
+                
+                summaryScore = response["attributeScores"]["TOXICITY"]["summaryScore"]["value"]
+
+            # checking for errors returned from the API itself
+            except Error as excObj:
+                if str(excObj) == "COMMENT_TOO_LONG":
+                    print("text is too long")
+                elif str(excObj) == "COMMENT_EMPTY":
+                    print("text is empty")
+                elif str(excObj) == "NO_REQUESTED_ATTRIBUTES":
+                    print("the attribute requested is not there")
+                elif str(excObj) == "LANGUAGE_NOT_SUPPORTED_BY_ATTRIBUTE":
+                    print("language is not supported")
+                elif str(excObj) == "LANGUAGE_DETECTION_FAILED":
+                    print("failed to detect the language")
+
+            # any other error, we assign a value of Zero to the row
+            except:
+                print("skipping tweet number ", rowID)
+                summaryScore = 0
+                print("assigning tweet number ", rowID, " a score of Zero")
+            
+            # comment out the line below if you do not want to see output on the screen
+            print(rowID, columnContainTheText, summaryScore)
+            
+            # creating a list of values (a row) 
+            CSVOutputRow = [rowID, columnContainTheText, summaryScore]
+
+            # adding the row to the output list
+            CSVOutputList.append(CSVOutputRow)
+
+            # making a request every one second 
+            # to avoid hitting rate limit
+            time.sleep(1)
+
+    # send the list to the function to creat the CSV output file.
+    write_output_to_CSV(CSVOutputList)
+        
+if __name__ == "__main__":
+    main()
